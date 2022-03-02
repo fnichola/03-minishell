4879a3a751de47d0d31ed2dc5efbeff17dff1e1b
Nic Flenghi <nic.flenghi@gmail.com>
2022-03-02 15:15:00 +0900

    move to school computer

--- srcs/main.c
+++ srcs/main.c
@@ -6,29 +6,40 @@
 /*   By: fnichola <fnichola@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:46:58 by fnichola          #+#    #+#             */
-/*   Updated: 2022/02/26 21:15:14 by fnichola         ###   ########.fr       */
+/*   Updated: 2022/02/28 21:16:26 by fnichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
+#include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
+char	**parse_command(char *line)
+{
+
+}
 
 int	main(int argc, char **argv)
 {
 	char	*line;
+	char	**args;
+	int		status;
+
 	(void)argc;
 	(void)argv;
 
-	while (1)
+	// initialize_minishell();
+	status = 1;
+	while (status)
 	{
 		line = readline("minishell$ ");
 		if (line && *line)
 			add_history(line);
+		args = parse_command(line);
+		// status = execute_command();
 		if (!ft_strncmp(line, "exit", ft_strlen(line)))
 			break ;	
-		printf("%s\n", line);
 		free(line);
 	}
 	free(line);
