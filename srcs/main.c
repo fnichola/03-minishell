8ecc6b830e79518f6ff271ca52bbd87669bffc79
Nic Flenghi <nic.flenghi@gmail.com>
2022-08-19 16:29:57 +0900

    refactor execute_commands, starting to make str-func table for looking up builtin functions

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,4 @@
 /* ************************************************************************** */
-
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -7,7 +6,7 @@
 /*   By: fnichola <fnichola@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:46:58 by fnichola          #+#    #+#             */
-/*   Updated: 2022/08/17 11:41:15 by fnichola         ###   ########.fr       */
+/*   Updated: 2022/08/19 16:27:26 by fnichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +19,23 @@
 #include <sys/wait.h>
 
 // t_minishell_data	g_data; //ここにあっても他のファイルで使えないから、minishell.hで定義する
+
+void	init_built_in_table(void)
+{
+	const t_str_func_table	temp[] = {
+		{"echo",	&built_in_echo},
+		{"cd",		&built_in_cd},
+		{"pwd",		&built_in_pwd},
+		{"env",		&built_in_env},
+		{"export",	&built_in_export},
+		{"exit",	&built_in_exit},
+	};
+	t_str_func_table	*built_in_func_table;
+
+	built_in_func_table = malloc_error_check(sizeof(temp));
+	ft_memcpy(built_in_func_table, temp, sizeof(temp));
+	g_data.built_in_func_table = built_in_func_table;
+}
 
 size_t	argv_len(char **argv)
 {
@@ -47,7 +63,7 @@ void	free_command_table(void *ptr)
 	free(command);
 }
 
-void	builtin_exit(int argc, char **argv)
+void	built_in_exit(int argc, char **argv)
 {
 	int	ret;
 
@@ -65,10 +81,24 @@ void	builtin_exit(int argc, char **argv)
 	}
 	else
 	{
-		printf("too many arguments\n");	//need a separate error function
+		printf("too many arguments\n"); //need a separate error function
 	}
 }
 
+void	close_exec_fds(void)
+{
+	size_t	i;
+
+	i = 0;
+	while (g_data.exec_fds[i])
+	{
+		if ((g_data.exec_fds[i])[0] > STDOUT_FILENO)
+			close((g_data.exec_fds[i])[0]);
+		if ((g_data.exec_fds[i])[1] > STDOUT_FILENO)
+			close((g_data.exec_fds[i])[1]);
+		i++;
+	}
+}
 // void free_token_list (void *ptr)
 // {
 // 	t_token	*token;
@@ -104,228 +134,158 @@ void	search_path_and_exec(char **argv, char **envp)
 	exit_error("Can't find command.");
 }
 
-// void	execute_built_in(char **argv, char **envp)
-// {
-
-// }
-
-// pid_t	execute_simple_command(char **argv, char **envp, int **exec_fds, t_envlist *e_list, int i)
-// {
-// 	pid_t	pid;
-// 	// int		i;
-
-// 	// i = 0;
-// 	if (!ft_strncmp(argv[0], "exit", \
-// 	ft_strlen(argv[0])) && ft_strlen(argv[0]) >= 4)
-// 	{
-// 		builtin_exit(argv_len(argv), argv);
-// 	}
-// 	pid = fork();
-// 	if (pid == 0)// 子プロセス
-// 	{
-// 		if (i != 0)
-// 		{
-// 			dup2(exec_fds[i - 1][0], STDIN_FILENO);
-// 		}
-// 		if (i == 0)
-// 		{
-// 			dup2(exec_fds[i][1], 1);//この後の処理が不明。ここでpipeに出力を書き込まれる
-// 			close(exec_fds[i][1]);
-// 		}
-// 		printf("子プロセス\n");
-// 		// close(exec_fds.in_fd);
-// 		// close(exec_fds.out_fd);
-// 		// close(exec_fds.pipe_fd[0]);
-// 		// close(exec_fds.pipe_fd[1]);
-// 		if (!ft_strncmp(argv[0], "echo", ft_strlen(argv[0])))
-// 		{
-// 			built_in_echo(argv, e_list);
-// 		}
-// 		else if (!ft_strncmp(argv[0], "cd", ft_strlen(argv[0])))
-// 		{
-// 			built_in_cd(argv, e_list);
-// 		}
-// 		else if (!ft_strncmp(argv[0], "pwd", ft_strlen(argv[0])))
-// 		{
-// 			built_in_pwd();
-// 		}
-// 		else if (!ft_strncmp(argv[0], "env", ft_strlen(argv[0])))
-// 		{
-// 			built_in_env(e_list);
-// 		}
-// 		else if (!ft_strncmp(argv[0], "export", ft_strlen(argv[0])))
-// 		{
-// 			built_in_export(argv, e_list);
-// 		}
-// 		else if (ft_strchr(argv[0], '/'))
-// 			execve(argv[0], argv, envp);
-// 		else
-// 			search_path_and_exec(argv, envp);
-// 	}
-// 	else
-// 	{
-// 		close(exec_fds[i][0]);
-// 		printf("親プロセス\n");
-// 	}
-// 	return (pid);
-// }
-
-void	execute_last_command(char **argv, char **envp, int **exec_fds, t_envlist *e_list, int i, int num_cmds)
+bool	execute_built_in(char **argv, char **envp)
 {
+	bool	is_builtin;
+	int		old_fd[2];
+
+	is_builtin = true;
+	old_fd[0] = dup(STDIN_FILENO);
+	old_fd[1] = dup(STDOUT_FILENO);
+	dup2((g_data.exec_fds[g_data.cmd_index])[0], STDIN_FILENO);
+	dup2((g_data.exec_fds[g_data.cmd_index])[1], STDOUT_FILENO);
+
+	size_t	i = 0;
+	printf("state func table 0 %s\n", g_data.built_in_func_table[0].name);
+
+
+
+	if (!ft_strncmp(argv[0], "echo", ft_strlen(argv[0])))
+		built_in_echo(argv);
+	else if (!ft_strncmp(argv[0], "cd", ft_strlen(argv[0])))
+		built_in_cd(argv, g_data.env_list);
+	else if (!ft_strncmp(argv[0], "pwd", ft_strlen(argv[0])))
+		built_in_pwd();
+	else if (!ft_strncmp(argv[0], "env", ft_strlen(argv[0])))
+		built_in_env(g_data.env_list);
+	else if (!ft_strncmp(argv[0], "export", ft_strlen(argv[0])))
+		built_in_export(argv, g_data.env_list);
+	else if (!ft_strncmp(argv[0], "exit", \
+	ft_strlen(argv[0])) && ft_strlen(argv[0]) >= 4)
+	{
+		built_in_exit(argv_len(argv), argv);
+	}
+	else
+		is_builtin = false;
+	dup2(old_fd[0], STDIN_FILENO);
+	close(old_fd[0]);
+	dup2(old_fd[1], STDOUT_FILENO);
+	close(old_fd[1]);
+	return (is_builtin);
+}
+
+void	execute_external(char **argv, char **envp)
+{
+	pid_t	pid;
 	int		status;
-	pid_t	pid;
+	size_t	i;
 
-	printf("last\n");
-	printf("num_cmds %d\n",num_cmds);
-
-	if (!ft_strncmp(argv[0], "exit", \
-	ft_strlen(argv[0])) && ft_strlen(argv[0]) >= 4)
-	{
-		builtin_exit(argv_len(argv), argv);
-	}
 	pid = fork();
 	if (pid == 0)// 子プロセス
 	{
-		if (num_cmds > 1)
-		{
-			printf("argv[1]\n");
-			if (i != 0)
-			{
-				dup2(exec_fds[i - 1][0], STDIN_FILENO);
-				close(exec_fds[i - 1][1]);
-			}
-			else
-			{
-				dup2(exec_fds[i][1], 1); //この後の処理が不明。ここでpipeに出力を書き込まれる
-				close(exec_fds[i][1]);
-			}
-			close(exec_fds[i][1]);
-		}
-		printf("子プロセス\n");
-		if (!ft_strncmp(argv[0], "echo", ft_strlen(argv[0])))
-			built_in_echo(argv);
-		else if (!ft_strncmp(argv[0], "cd", ft_strlen(argv[0])))
-			built_in_cd(argv, e_list);
-		else if (!ft_strncmp(argv[0], "pwd", ft_strlen(argv[0])))
-			built_in_pwd();
-		else if (!ft_strncmp(argv[0], "env", ft_strlen(argv[0])))
-			built_in_env(e_list);
-		else if (!ft_strncmp(argv[0], "export", ft_strlen(argv[0])))
-			built_in_export(argv, e_list);
-		else if (ft_strchr(argv[0], '/'))
+		dup2((g_data.exec_fds[g_data.cmd_index])[0], STDIN_FILENO);
+		dup2((g_data.exec_fds[g_data.cmd_index])[1], STDOUT_FILENO);
+		close_exec_fds();
+		if (ft_strchr(argv[0], '/'))
 			execve(argv[0], argv, envp);
 		else
 			search_path_and_exec(argv, envp);
 	}
 	else
 	{
-		close(exec_fds[i][1]);
-		printf("親プロセス\n");
+		if (g_data.cmd_index == g_data.num_cmds - 1)
+		{
+
+			printf("close fds after executing last command\n");
+			close_exec_fds();
+			waitpid(pid, &status, WUNTRACED);
+		}
 	}
-	waitpid(pid, &status, WUNTRACED);
 }
 
-void	execute_piped_command(char **argv, char **envp, int **exec_fds, t_envlist *e_list, int i, int num_cmds)
+void	execute_simple_command(char **argv, char **envp)
 {
-	printf("piped\n");
-	printf("num_cmds %d\n", num_cmds);
-	printf("exec_fds[0] %d\n", exec_fds[i][0]);
-	pid_t	pid;
+	if (execute_built_in(argv, envp))
+		return ;
+	else
+		execute_external(argv, envp);
+}
 
-	if (!ft_strncmp(argv[0], "exit", \
-	ft_strlen(argv[0])) && ft_strlen(argv[0]) >= 4)
+
+void	connect_pipes()
+{
+	int		pipe_fd[2];
+	size_t	i;
+
+	g_data.exec_fds = malloc_error_check(sizeof(int *) * (g_data.num_cmds + 1));
+	i = 0;
+	while (i < g_data.num_cmds)
 	{
-		builtin_exit(argv_len(argv), argv);
+		g_data.exec_fds[i] = malloc_error_check(sizeof(int) * 2);
+		i++;
 	}
-	pid = fork();
-	if (pid == 0)// 子プロセス
+	g_data.exec_fds[i] = NULL;
+	if (g_data.num_cmds == 1)
 	{
-		if (i != 0)
-		{
-			dup2(exec_fds[i][1], 1);
-			dup2(exec_fds[i - 1][0], STDIN_FILENO);
-			close(exec_fds[i - 1][1]);
-		}
-		if (i == 0)
-		{
-			dup2(exec_fds[i][1], 1);//この後の処理が不明。ここでpipeに出力を書き込まれる
-			close(exec_fds[i][1]);
-		}
-		// if ()
-		printf("子プロセス\n");
-		if (!ft_strncmp(argv[0], "echo", ft_strlen(argv[0])))
-		{
-			built_in_echo(argv);
-		}
-		else if (!ft_strncmp(argv[0], "cd", ft_strlen(argv[0])))
-		{
-			built_in_cd(argv, e_list);
-		}
-		else if (!ft_strncmp(argv[0], "pwd", ft_strlen(argv[0])))
-		{
-			built_in_pwd();
-		}
-		else if (!ft_strncmp(argv[0], "env", ft_strlen(argv[0])))
-		{
-			built_in_env(e_list);
-		}
-		else if (!ft_strncmp(argv[0], "export", ft_strlen(argv[0])))
-		{
-			built_in_export(argv, e_list);
-		}
-		else if (ft_strchr(argv[0], '/'))
-			execve(argv[0], argv, envp);
-		else
-			search_path_and_exec(argv, envp);
-		close(exec_fds[i][1]);
-		close(exec_fds[i][0]);
+		(g_data.exec_fds[0])[0] = STDIN_FILENO;
+		(g_data.exec_fds[0])[1] = STDOUT_FILENO;
 	}
 	else
 	{
-		close(exec_fds[i][1]);
-		printf("親プロセス\n");
+		(g_data.exec_fds[0])[0] = STDIN_FILENO;
+		i = 1;
+		while (i < g_data.num_cmds)
+		{
+			ft_wpipe(pipe_fd);
+			printf("pipe_fd = %d, %d\n", pipe_fd[0], pipe_fd[1]);
+			(g_data.exec_fds[i - 1])[1] = pipe_fd[1];
+			(g_data.exec_fds[i])[0] = pipe_fd[0];
+			i++;
+		}
+		i--;
+		(g_data.exec_fds[i])[1] = STDOUT_FILENO;
+	}
+	i = 0;
+	while (i < g_data.num_cmds)
+	{
+		printf("exec_fds[%zu] = %d, %d\n", i, (g_data.exec_fds[i])[0], (g_data.exec_fds[i])[1]);
+		i++;
 	}
 }
 
-int	execute_commands(char **envp, t_envlist *e_list)
+int	execute_commands(char **envp)
 {
 	t_command	*command;
 	t_list		*command_table_ptr;
 	char		**argv;
-	int			num_cmds;
 	int			i;
 	int			**exec_fds;
 
 	i = -1;
 	command_table_ptr = g_data.command_table;//ここでグローバル変数から、コマンドを代入している
-	num_cmds = ft_lstsize(g_data.command_table);
-	exec_fds = (int **)malloc_error_check(sizeof(int *) * num_cmds);
-	while (++i < num_cmds)
-		exec_fds[i] = (int *)malloc_error_check(sizeof(int) * 2);
-	i = -1;
-	while (++i < num_cmds)
-		ft_wpipe(exec_fds[i]);
-	i = 0;
-	while (i < num_cmds)//num_cmdsはパイプがあれば、増えていく
+	g_data.num_cmds = ft_lstsize(g_data.command_table);
+	connect_pipes();
+
+	g_data.cmd_index = 0;
+	while (g_data.cmd_index < g_data.num_cmds)//num_cmdsはパイプがあれば、増えていく
 	{
-		printf("start\n");
-		printf("num_cmds = %d\n", num_cmds);
+		// printf("num_cmds = %d\n", num_cmds);
 		command = (t_command *)command_table_ptr->content;
 		argv = command->argv;//ここでargvにcommandのargvが代入されているので、built_inにはargvを渡せばいい。
 		if (!argv || !argv[0])
 			return (0);
-		if (i == num_cmds - 1) // last simple command
-			execute_last_command(argv, envp, exec_fds, e_list, i, num_cmds);
-		else
-			execute_piped_command(argv, envp, exec_fds, e_list, i, num_cmds);
+		execute_simple_command(argv, envp);
 		command_table_ptr = command_table_ptr->next;
+		g_data.cmd_index++;
+	}
+	i = 0;
+	while(g_data.exec_fds[i])
+	{
+		free(g_data.exec_fds[i]);
 		i++;
 	}
-	i = -1;
-	while(++i < num_cmds)
-		free(exec_fds[i]);
-	free(exec_fds);
-	exec_fds = NULL;
+	free(g_data.exec_fds);
+	g_data.exec_fds = NULL;
 	return (0);
 }
 
@@ -333,11 +293,11 @@ int	minishell(char **envp)
 {
 	int			status;
 	char		*line;
-	t_envlist	*env_list;
 	t_list		*tokens;
 
-	env_list = init_env_list(envp);
+	g_data.env_list = init_env_list(envp);
 	g_data.command_table = NULL;
+	init_built_in_table();
 	status = 0;
 	printf("minishell\n");
 	while (!status)
@@ -347,11 +307,11 @@ int	minishell(char **envp)
 			add_history(line);
 		tokens = tokenizer(line);
 		// printf("tokens %s\n", (char *)tokens->next->content);
-		g_data.command_table = parser(tokens, env_list);
+		g_data.command_table = parser(tokens, g_data.env_list);
 		// printf("g_data.command_table %s\n", (char *)g_data.command_table->content->);
 		free(line);
 		printf("before execute\n");
-		status = execute_commands(envp, env_list);
+			status = execute_commands(envp);
 		ft_lstclear(&g_data.command_table, free_command_table);
 	}
 	return (0);
